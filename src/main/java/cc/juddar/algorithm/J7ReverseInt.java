bdfa9d1e81fe7dbe79ce3d5acb9588d49b3c3eeb
dasongju <lnhjds@126.com>
2018-08-28 11:12:47 +0800

    J7

--- src/main/java/cc/juddar/algorithm/J7ReverseInt.java
+++ src/main/java/cc/juddar/algorithm/J7ReverseInt.java
@@ -1,5 +1,27 @@
 package cc.juddar.algorithm;
 
+/**
+ * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+ *
+ */
 public class J7ReverseInt {
 
+  public static int reverse(int x) {
+    int rev = 0;
+
+    while (x != 0) {
+      int pop = x % 10;//获取最后一位
+      x /= 10;//取完最后一位x变成x/10之后的数
+      if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+      if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+      rev = rev * 10 + pop;
+    }
+    return rev;
+  }
+
+  public static void main(String[] args) {
+    System.err.println(reverse(2310));
+    System.err.println(reverse(-8930));
+    System.err.println(reverse(Integer.MAX_VALUE));
+  }
 }
