a2aa1af8494b0ea6526ebc4ed29a8da48a602a7f
dasongju <lnhjds@126.com>
2018-09-18 10:46:03 +0800

    J20 有效字符串

---
+++ src/main/java/cc/juddar/algorithm/J20IsValid.java
@@ -0,0 +1,50 @@
+package cc.juddar.algorithm;
+
+import java.util.Stack;
+
+/**
+ * @Description给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。 左括号必须以正确的顺序闭合。 注意空字符串可被认为是有效字符串。
+ * @Author dasongju
+ * @Date 2018/9/18 上午10:28
+ */
+public class J20IsValid {
+
+    /**
+     * @Description 1 个数必须为偶数了，
+     * 一听就low，为啥我脑子里只有枚举
+     * @Author dasongju
+     * @Date 上午10:31 2018/9/18
+     * @Param
+     **/
+    public boolean isValid(String s) {
+        Stack<Character> validStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                validStack.push(s.charAt(i));
+            } else {
+                if (validStack.empty()) {//处理了个数是奇数的情景了
+                    return false;
+                }
+                if (s.charAt(i) == ')' && validStack.lastElement() != '(') {
+                    return false;
+                }
+                if (s.charAt(i) == '}' && validStack.lastElement() != '{') {
+                    return false;
+                }
+                if (s.charAt(i) == ']' && validStack.lastElement() != '[') {
+                    return false;
+                }
+                validStack.pop();
+            }
+        }
+        return validStack.empty();
+    }
+
+    public static void main(String[] args) {
+        System.err.println(new J20IsValid().isValid("()"));
+    }
+}
