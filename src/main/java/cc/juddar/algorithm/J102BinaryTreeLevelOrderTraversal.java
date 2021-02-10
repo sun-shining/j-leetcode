40352b1bfa0ecec4fb6025f1e7c68b17c0de6ba5
judasong <judasong@58.com>
2021-02-10 22:06:30 +0800

    BFS DFS

--- src/main/java/cc/juddar/algorithm/J102BinaryTreeLevelOrderTraversal.java
+++ src/main/java/cc/juddar/algorithm/J102BinaryTreeLevelOrderTraversal.java
@@ -10,6 +10,8 @@ import java.util.Queue;
 
 /**
  * 给定一个二叉树，将每层数据遍历到数组中
+ * BFS-广度优先遍历
+ * DFS-深度优先遍历
  * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
  *
  * For example:
@@ -46,20 +48,29 @@ public class J102BinaryTreeLevelOrderTraversal {
         String s = Arrays.toString(lists.toArray());
         System.out.println("s = " + s);
 
+        List<List<Integer>> result = new ArrayList<>();
+        traversalWithDFS(root, 0, result);
+        System.out
+            .println("Arrays.toString(result.toArray()) = " + Arrays.toString(result.toArray()));
+
     }
 
+    /**
+     *  难点1：怎么知道循环到的那一层到头了？在内层循环中一次性将同一层的元素全部取出
+     *  难点2：同一层的处理体现在依次加入到queue中
+     **/
     private static List<List<Integer>>  traversalWithBFS(TreeNode root) {
         if (root == null) {
             return Collections.emptyList();
         }
         List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue =  new LinkedList<>();
         queue.add(root);
 
         while (!queue.isEmpty()) {
-            int curLevel = queue.size();
+            int curLevelSize = queue.size();
             List<Integer> curList = new ArrayList<>();
-            for (int i = 0; i < curLevel; i++) {
+            for (int i = 0; i < curLevelSize; i++) {
                 TreeNode curNode = queue.poll();
                 curList.add(curNode.val);
                 if (curNode.left != null) {
@@ -70,8 +81,33 @@ public class J102BinaryTreeLevelOrderTraversal {
                 }
             }
             result.add(curList);
-
         }
-        return result;
+            return result;
+    }
+
+    /**
+     *  深度优先遍历
+     *  难点1：需要有level的概念，告诉递归现在在哪一层
+     *  难点2：首次进入某层时需要赛个空集合,以防后面操作空指针
+     **/
+    private static void traversalWithDFS(TreeNode node, int level, List<List<Integer>> result) {
+        if (node == null) {
+            return;
+        }
+
+        // 重要
+        if (result.size() == level) {
+            result.add(level, new ArrayList<>());
+        }
+
+        result.get(level).add(node.val);
+
+        if (node.left != null) {
+            traversalWithDFS(node.left,level+1, result);
+        }
+        if (node.right != null) {
+            traversalWithDFS(node.right,level+1,result);
+        }
+
     }
 }
